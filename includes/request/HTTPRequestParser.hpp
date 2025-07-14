13d54d282494da118a6292f51c935d06e612f7f7
Ondřej Běhávka <ondbeh@gmail.com>
2025-07-14 21:42:22 +0200

    Implementation of the isValidRequest method. ☑️ Implemented the header parser for parsing HTTP requests, used it in the validity checker. Might add more validation checks later.

--- includes/request/HTTPRequestParser.hpp
+++ includes/request/HTTPRequestParser.hpp
@@ -1,9 +1,16 @@
 #pragma once
 
 #include "HTTPRequestData.hpp"
+#include "utils.hpp"
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <sstream>
 
 class HTTPRequestParser
 {
+private:
+    static std::unordered_map<std::string, std::string> parseHeaders(std::istringstream headerStream);
 public:
     static bool isValidRequest(const std::string &request_str);
     static HTTPRequestData parse(const std::string &request_str);
--- src/request/HTTPRequestParser.cpp
+++ src/request/HTTPRequestParser.cpp
@@ -1,9 +1,22 @@
 #include "HTTPRequestParser.hpp"
 
-bool HTTPRequestParser::isValidRequest(const std::string &request_str)
+bool HTTPRequestParser::isValidRequest(const std::string &buffer)
 {
-    (void)request_str; // Placeholder for actual validation logic
-    // TODO: Implement validation logic
+    auto bodyStart = buffer.find("\r\n\r\n");
+    if (bodyStart == std::string::npos)
+        return false;
+    std::istringstream headerStream(buffer.substr(0, bodyStart));
+    bodyStart += 4; // Skip CRLF CRLF
+    auto headers = parseHeaders(std::move(headerStream));
+    if (headers.find("Content-Length") != headers.end())
+    {
+        auto contentLength = std::stoul(headers["Content-Length"]);
+        if (bodyStart + contentLength > buffer.size())
+            return false;
+    }
+    if (headers.find("Transfer-encoding") != headers.end()
+        && headers["Transfer-encoding"] == "chunked")
+        return buffer.find("0\r\n\r\n", bodyStart) != std::string::npos;
     return true;
 }
 
@@ -19,4 +32,23 @@ HTTPRequestData HTTPRequestParser::parse(const std::string &request_str)
         ""
     };
     return data;
+}
+
+std::unordered_map<std::string, std::string> HTTPRequestParser::parseHeaders(
+    std::istringstream headerStream)
+{
+    std::unordered_map<std::string, std::string> headers;
+    std::string line;
+
+    while (std::getline(headerStream, line)) {
+        size_t colon_pos = line.find(':');
+        if (colon_pos != std::string::npos) {
+            std::string key = line.substr(0, colon_pos);
+            std::string value = line.substr(colon_pos + 1);
+            trim(key);
+            trim(value);
+            headers[key] = value;
+        }
+    }
+    return headers;
 }
