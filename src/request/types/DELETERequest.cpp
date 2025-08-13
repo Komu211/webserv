4494a347a284070e5e82accc28cb5b71b16388d9
Komu211 <75761278+Komu211@users.noreply.github.com>
2025-08-13 10:38:51 +0200

    Simple DELETE Method

--- src/request/types/DELETERequest.cpp
+++ src/request/types/DELETERequest.cpp
@@ -6,6 +6,36 @@ DELETERequest::DELETERequest(HTTPRequestData data, const LocationConfig* locatio
 
 std::string DELETERequest::handle()
 {
-    // Replace this with actual logic for handling GET requests
-    return "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\ncontent-length: 13\r\n\r\nHello, World!";
+    // Check method allowed
+    if (!_effective_config->getLimitExcept().empty() &&
+        _effective_config->getLimitExcept().find("delete") == _effective_config->getLimitExcept().end())
+    {
+        return errorResponse(405);
+    }
+
+    // Redirection handling
+    if (_effective_config->getReturn().first != -1)
+        return handleRedirection(_effective_config->getReturn());
+
+    // Resolve target resource path
+    std::filesystem::path targetPath{_effective_config->getRoot()};
+    targetPath /= getURInoLeadingSlash();
+
+    if (!std::filesystem::exists(targetPath))
+        return errorResponse(404);
+
+    if (std::filesystem::is_directory(targetPath))
+        return errorResponse(403);
+
+    // Remove the file
+    std::error_code ec;
+    bool removed = std::filesystem::remove(targetPath, ec);
+    if (ec)
+        return errorResponse(500);
+
+    if (!removed)
+        return errorResponse(404);
+
+    ResponseWriter response(200, {{"Content-Type", "text/plain"}}, std::string("Deleted \"") + targetPath.filename().string() + "\"\n");
+    return response.write();
 }
